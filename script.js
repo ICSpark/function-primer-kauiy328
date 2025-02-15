8a729c5497510ab41af7383d485b72bcf4a18812
kauiy328 <kauiy38711@gmail.com>
2025-02-15 20:01:43 +0000

    Live Code Functions Completed

---
+++ script.js
@@ -0,0 +1,24 @@
+function functionInitiate() {
+    console.log("********************");
+    console.log("PROGRAM INITIALIZED");
+    console.log("********************");
+}
+
+function bankworth(dollars) {
+    console.log("$ "+ dollars +" is worth " + dollars * 4 + " quarters")
+}
+
+functionInitiate()
+
+bankworth(5);
+
+bankworth(10);
+
+
+functionInitiate()
+
+bankworth(2);
+
+functionInitiate()
+
+bankworth(0);
